fef5f73e4fd4926c0473c0a7c931aa7cdeac3a7f
tejash <tejashyadavinfo@gmail.com>
2025-11-09 13:27:07 +0530

    feat: add library api

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from todo.views import TodoViewSet
+from library.views import BookViewSet
 
 router = DefaultRouter()
 router.register('todos', TodoViewSet, basename='todos')
+router.register('books', BookViewSet, basename='books')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
